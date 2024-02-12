e03e6f0f8dc843b8826b64546f1cae4de4dc9b7d
Diego Braga <diebraga0055@gmail.com>
2024-02-12 20:23:38 +0000

    Contract

---
+++ contract/scripts/deploy.ts
@@ -0,0 +1,27 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Trasactions = await ethers.getContractFactory("Transactions");
+  const transactions = await Trasactions.deploy();
+
+  await transactions.waitForDeployment();
+  const receipt = await transactions.deploymentTransaction()?.wait();
+
+  console.log(
+    `Trasactions contract deployed to ` + (await transactions.getAddress())
+  );
+
+  console.log(`Gas used for deployment: ${receipt?.gasPrice.toString()}`);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
