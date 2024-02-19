3d1b645c55ad1474be36d3984c8707fe57c2aee3
Diego Braga <diebraga0055@gmail.com>
2024-02-19 21:27:33 +0000

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
-import { Route, Routes } from "react-router-dom";
-import {
-  Home,
-  Navbar,
-  Sidebar,
-  CampaignDetails,
-  CreateCampaign,
-  Profile,
-} from "./components";
+import { Sidebar, Navbar } from "./components";
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-export default function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+  },
+  {
+    path: "create-campaign",
+    element: <CreateCampaign />,
+  },
+  {
+    path: "campaign-details/:id",
+    element: <CampaignDetails />,
+  },
+]);
+
+const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
@@ -18,13 +31,10 @@ export default function App() {
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-campaign" element={<CreateCampaign />} />
-          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
-        </Routes>
+        <RouterProvider router={router} />
       </div>
     </div>
   );
-}
+};
+
+export default App;
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,8 +1,4 @@
-import CampaignDetails from "./CampaignDetails";
-import CreateCampaign from "./CreateCampaign";
-import Home from "./Home";
 import Navbar from "./Navbar";
-import Profile from "./Profile";
 import Sidebar from "./Sidebar";
 
-export { CampaignDetails, CreateCampaign, Home, Navbar, Profile, Sidebar };
+export { Navbar, Sidebar };
