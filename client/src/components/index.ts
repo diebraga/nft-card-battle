f940f4d52f8e9d0a7e263278be00cb70c0c9e694
Diego Braga <diebraga0055@gmail.com>
2024-03-10 19:12:12 +0000

    create campaign

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Layout from "./Layout";
 import Button from "./Button";
+import FormField from "./FormField";
 
-export { Navbar, Sidebar, Layout, Button };
+export { Navbar, Sidebar, Layout, Button, FormField };
